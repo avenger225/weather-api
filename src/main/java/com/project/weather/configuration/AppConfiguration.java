0ac9e1a409e6e5f1d5a488747911b5be7c33799b
MH <avenger225@o2.pl>
2021-02-26 16:48:53 +0100

    Added swagger 2

--- src/main/java/com/project/weather/configuration/AppConfiguration.java
+++ src/main/java/com/project/weather/configuration/AppConfiguration.java
@@ -6,9 +6,11 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableCaching
+@EnableSwagger2
 public class AppConfiguration {
 
     @Bean
