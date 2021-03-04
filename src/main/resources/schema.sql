d834f4a4420a6b0d44ac11b445669aa40741f090
MH <avenger225@o2.pl>
2021-03-04 12:43:19 +0100

    Fix for script

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,13 +7,13 @@ create table surfing_location (
 
 create table users (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    username varchar ( 50 ) not null UNIQUE,
-    password varchar ( 100 ) not null,
+    username varchar (50) not null UNIQUE,
+    password varchar (100) not null,
     enabled boolean not null
 );
 create table authorities (
-    username varchar ( 50 ) not null,
-    authority varchar ( 50 ) not null,
+    username varchar (50) not null,
+    authority varchar (50) not null,
     constraint fk_authorities_users foreign key (username) references users(username),
     UNIQUE KEY username_authority (username, authority)
 );
