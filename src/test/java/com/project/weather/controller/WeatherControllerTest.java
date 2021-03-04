f13b0aa24a91fc6e448b62788c1f994ea1aa69e9
MH <avenger225@o2.pl>
2021-03-04 16:56:58 +0100

    Added weather controller test

--- src/test/java/com/project/weather/controller/WeatherControllerTest.java
+++ src/test/java/com/project/weather/controller/WeatherControllerTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -14,40 +15,28 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class LoginControllerTest {
-
+@WithMockUser
+public class WeatherControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
+
     @Test
-    void shouldLoginAndGetContent() throws Exception {
-        MvcResult login = mockMvc.perform(post("/login")
-                .content("{\"login\": \"test\", \"password\": \"test\"}"))
+    public void getSurfingLocations() throws Exception {
+        MvcResult result = mockMvc.perform(get("/weather/surfing"))
                 .andDo(print())
                 .andExpect(status().is(200))
                 .andReturn();
-        String token = login.getResponse().getHeader("Authorization");
 
-        mockMvc.perform(get("/login/test")
-                .header("Authorization", token))
-                .andDo(print())
-                .andExpect(status().is(200))
-                .andExpect(content().string("test"));
-
-    }
-
-    @Test
-    void loginShouldFailedWithUnknownCredentials () throws Exception {
-        mockMvc.perform(post("/login")
-                .content("{\"login\": \"test123\", \"password\": \"test123\"}"))
-                .andDo(print())
-                .andExpect(status().is(401));
+        List<Forecast> forecastList = Arrays.asList(objectMapper.readValue(result.getResponse().getContentAsString(), Forecast[].class));
+        assertThat(forecastList).isNotNull();
     }
 }
